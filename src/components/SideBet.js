f22bdaa44ad51fb9d98be849e6d85ac263f79176
Ranjeet2610 <53467396+Ranjeet2610@users.noreply.github.com>
2021-03-07 23:56:12 +0530

    reverse

--- src/components/SideBet.js
+++ src/components/SideBet.js
@@ -917,7 +917,7 @@ export default class SideBet extends Component {
     const indexOfLastPost = this.state.currentPage * this.state.postsPerPage;
     const indexOfFirstPost = indexOfLastPost - this.state.postsPerPage;
     const currentPosts = this.state.betHistroy?.reverse().slice(indexOfFirstPost, indexOfLastPost);
-    const fcurrentPosts = this.state.fbetHistroy?.reverse().slice(indexOfFirstPost, indexOfLastPost);
+    //const fcurrentPosts = this.state.fbetHistroy?.reverse().slice(indexOfFirstPost, indexOfLastPost);
 
     return (
     <div className="col-md-4 col-xs-12">
