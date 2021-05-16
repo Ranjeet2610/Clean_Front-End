a9c39cf56493fbce876b9a8e0d6ac5df3447969d
ranjeet <ranjeetsuneja26@gmail.com>
2021-05-16 15:20:47 +0530

    server ip

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 
 // export const APIURL = "http://18.134.128.178:4000/api/"
-export const APIURL = "http://65.1.151.149:4000/api/"
+export const APIURL = "http://3.108.46.86:4000/api/"
 
 // export const APIURL = "http://65.0.100.189:4000/api/"
 
