36ff81e97c0e44ce9e69f5fac35feb3b7bf295c6
Ranjeet2610 <53467396+Ranjeet2610@users.noreply.github.com>
2021-06-16 12:07:06 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 
 // export const APIURL = "http://18.134.128.178:4002/api/"
-export const APIURL = "http://3.108.46.86:4000/api/"
+export const APIURL = "http://3.108.95.93:4000/api/"
 
 // export const APIURL = "http://65.0.100.189:4002/api/"
 
